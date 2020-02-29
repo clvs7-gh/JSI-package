60aa2ca0e30a6f0669181a59d750d1d47b2386a9
clvs7 <me@clvs7.com>
2020-02-29 14:07:18 +0900

    Update JSI

--- test.js
+++ test.js
@@ -7,11 +7,13 @@ var result = [];
 for (i = 0; i < 10; ++i) {
     result.push(i*i);
 }
+sleep(1000);
 test(result.join(', '));
 `;
 
 const initFunc = (i, s) => {
   i.setProperty(s, 'test', i.createNativeFunction((text) => assert.strictEqual(text, '0, 1, 4, 9, 16, 25, 36, 49, 64, 81')));
+  i.setProperty(s, 'sleep', i.createAsyncFunction((timeMs, callback) => setTimeout(callback, timeMs)));
 };
 const myInterpreter = new Interpreter(myCode, initFunc);
 myInterpreter.run();
